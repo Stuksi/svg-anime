259c940d6e3265d60e10ff4645e27efa2dfa8c21
Konstantin <k.konstantinov.2000@gmail.com>
2023-02-05 00:53:39 +0200

    Add initial client view

---
+++ client/js/render.js
@@ -0,0 +1,21 @@
+const codeInput = document.querySelector('#code-input');
+const svgRender = document.querySelector('#svg-render');
+
+function validateSVG(svgCode) {
+  const svgRegex = /<svg(?:.*?)>(.*?)<\/svg>/s;
+  const match = svgRegex.exec(svgCode.trim());
+
+  if (!match) {
+    return '<p>Please input a valid SVG code</p>';
+  }
+
+  return match[0];
+}
+
+function renderSVG()
+{
+  svgRender.innerHTML = validateSVG(codeInput.value);
+}
+
+codeInput.addEventListener('input', renderSVG);
+document.addEventListener("DOMContentLoaded", renderSVG);
